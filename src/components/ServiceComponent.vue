e7232a71a14c4f13fb379386a13e899223bb8650
vladmeh <vladmeh@gmail.com>
2019-11-02 18:33:31 +0300

    add ServiceComponent

---
+++ src/components/ServiceComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="/images/fitness-1.jpg" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <h3 class="font-bold text-xl">The Coldest Sunset</h3>
+            <p class="text-sm text-gray-600 mb-2">Sub title</p>
+            <p class="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+        </div>
+        <div class="px-6 py-4">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ServiceComponent"
+    }
+</script>
---
+++ src/views/Service.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container mx-auto my-12">
+        <ServiceComponent/>
+    </div>
+</template>
+
+<script>
+    import ServiceComponent from "../components/ServiceComponent";
+    export default {
+        name: "Service",
+        components: {
+            ServiceComponent
+        }
+    }
+</script>
